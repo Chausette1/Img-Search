f25d1c1f710c225b7931d8ebdd88334606cb7a15
Ertil2 <theocharliermsm@gmail.com>
2023-11-07 15:52:14 +0100

    launcher et list file finit (normalement)

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-if [ $1 == '-a' ]; then
+if [[ -z "$1" ]]; then
+	echo "veuillez entrer un mode (-a / -automatic ou -i / -interactive)"
+	exit
+fi
+
+make
+
+if [[ "$1" == '-a' || "$1" == 'automatic' ]]; then
+	if [[ -z  "$2" || ! -e "$2" ]]; then
+		echo "veuillez entrer le chemin vers le dossier contenant les images"
+		exit
+	fi
 	
+	./img-search "$1" "$2"
 
 
-elif [ $1 == '-i' ]; then
-
-
+elif [[ "$1" == '-i' || "$1" == 'interactive' ]]; then
+	if [[ -e "$2" ]]; then
+		echo "Chemin invalide"
+		exit
+	fi
+	
+	if [[ -z "$2" ]]; then
+		./img-search "$1" ./img/
+	else
+		./img-search "$1" "$2"
+	fi
 
 else
 	echo "Mode non valide."
